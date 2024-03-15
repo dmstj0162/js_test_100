8d25a78c9c054cbf56212f2fe8d5c107a48e1f7b
dmstj0162 <dmstj0162@gmail.com>
2024-03-15 13:39:17 +0900

    배열을 매개변수의 범위로 자르기

---
+++ count.js
@@ -0,0 +1,12 @@
+let start_num;
+let end_num;
+
+main(3, 10)
+function main(start_num, end_num){
+    var answer = [];
+    for(let i = start_num; i <= end_num; i++){
+        answer.push(i);
+    }
+    
+    console.log(answer)
+}
