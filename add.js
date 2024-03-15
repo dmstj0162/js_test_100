ab911e261fc055afb0565b152489a4eda66b8665
dmstj0162 <dmstj0162@gmail.com>
2024-03-15 13:44:15 +0900

    add 함수 사용

---
+++ add.js
@@ -0,0 +1,21 @@
+// let a;
+// let b;
+
+// solution(4,5)
+// function solution(a,b){
+//     let c = a + b;
+//     console.log(`${a}+${b}=${c}`)
+// }
+
+let a;
+let b;
+
+// solution(4,5)
+// function solution(a,b){
+//     console.log(`a = ${a}\nb = ${b}`)
+// }
+main(3,4)
+function main(a,b){
+    let answer = a + b;
+    console.log(answer)
+}
